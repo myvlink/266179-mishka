133b030f236aa477d762d12f4b87122c86188288
myvlink <themyvlink@gmail.com>
2018-05-19 02:56:06 +0500

    js для меню

--- source/js/script.js
+++ source/js/script.js
@@ -15,3 +15,16 @@ function initMap() {
     icon: image
   });
 }
+
+// var navToggle = document.querySelector(".main-nav__toggle");
+// var navMain = document.querySelector(".main-nav");
+// navMain.classList.remove('main-nav--nojs');
+// navToggle.addEventListener('click', function() {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--closed');
+//     navMain.classList.remove('main-nav--opened');
+//   }
+// });
