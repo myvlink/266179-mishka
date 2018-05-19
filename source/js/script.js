4fc26d3307b88252e37240944182fffe74197e9f
myvlink <themyvlink@gmail.com>
2018-05-19 18:30:31 +0500

    разбивка на подпапки и normalize

--- source/js/script.js
+++ source/js/script.js
@@ -16,15 +16,15 @@ function initMap() {
   });
 }
 
-// var navToggle = document.querySelector(".main-nav__toggle");
-// var navMain = document.querySelector(".main-nav");
-// navMain.classList.remove("main-nav--nojs");
-// navToggle.addEventListener("click", function() {
-//   if (navMain.classList.contains("main-nav--closed")) {
-//     navMain.classList.remove("main-nav--closed");
-//     navMain.classList.add("main-nav--opened");
-//   } else {
-//     navMain.classList.add("main-nav--closed");
-//     navMain.classList.remove("main-nav--opened");
-//   }
-// });
+var navToggle = document.querySelector(".main-nav__toggle");
+var navMain = document.querySelector(".main-nav__list");
+navMain.classList.remove("main-nav__list--nojs");
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav__list--closed")) {
+    navMain.classList.remove("main-nav__list--closed");
+    navMain.classList.add("main-nav__list--opened");
+  } else {
+    navMain.classList.add("main-nav__list--closed");
+    navMain.classList.remove("main-nav__list--opened");
+  }
+});
